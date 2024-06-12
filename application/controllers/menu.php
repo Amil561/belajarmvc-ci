00ded783a59bd749dcad54c62847a3c811c94ddc
Amil561 <amilmaaruf@gmail.com>
2024-06-12 18:07:32 +0700

    pertemuan 11 gue

---
+++ application/controllers/menu.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Menu extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        $data['title'] = 'Menu Management';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('menu/index', $data);
+        $this->load->view('templates/footer');
+    }
+
+    public function submenu()
+    {
+        $data['title'] = 'Submenu Management';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('menu/submenu', $data);
+        $this->load->view('templates/footer');
+    }
+}
+?>
---
+++ application/views/menu/index.php
@@ -0,0 +1,8 @@
+<!-- Begin page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="H3 mb-4 text-gray-800"><?= $title; ?></h1>
+
+</div>
+<!-- /.container-fluid -->
